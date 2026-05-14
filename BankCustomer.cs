47ce7eeaf738e642553627dc0c757d33a79726c0
Good-E-Nuff <248106637+Jerry-B-Good@users.noreply.github.com>
2026-05-13 23:32:06 -0400

    Initial upload

---
+++ BankCustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_M1
+{
+    public class BankCustomer
+    {
+
+        private static int s_nextCustomerId;
+
+        // add public fields for FirstName, LastName, and CustomerId
+        public string FirstName = "Butters";
+        public string LastName = "Worth";
+        public readonly string CustomerId;
+
+        static BankCustomer()
+        {
+            Random random = new Random();
+            s_nextCustomerId = random.Next(10000000, 20000000);
+        }
+
+        public BankCustomer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            this.CustomerId = (s_nextCustomerId++).ToString("D10");
+        }
+
+
+    }
+}
